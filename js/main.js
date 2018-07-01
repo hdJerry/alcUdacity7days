14a38e338a1b7e40a2b04ef5fb63583894ed9a08
Hogan jerry <damilolajerryhd@gmail.com>
2018-07-01 22:12:28 +0100

    before deadline change, added flags

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
 "use stricks";
 let db;
 
+$( "#CURR_FR" )
+  .change(function() {
+    let str = "";
+    $( "#CURR_FR option:selected" ).each(function() {
+
+      str += $( this ).val() + " ";
+
+    });
+    let cls = str.slice(0,2).toLowerCase();
+    $("#flagFR").addClass('flag-icon-'+cls);
+  })
+  .trigger( "change" );
+
+
+  $( "#CURR_TO" )
+    .change(function() {
+      let st = "";
+      $( "#CURR_TO option:selected" ).each(function() {
+
+        st += $( this ).val() + " ";
+
+      });
+      let cl = st.slice(0,2).toLowerCase();
+      $("#flagTO").addClass('flag-icon-'+cl);
+    })
+    .trigger( "change" );
+
+
     /************************************
     Function to create db in the IndexedDB
     using ForerunnerDB.
@@ -8,16 +36,22 @@ let db;
 
 function init_db(){
      if(db == null || db == 'undefined'){
-       window.fdb = new ForerunnerDB();
-       db = fdb.db("AlcUdacity");
-       db.persist.driver("IndexedDB");
+        window.fdb = new ForerunnerDB();
+        db = fdb.db("AlcUdacity");
+        db.persist.driver("IndexedDB");
 
-     }
-     db_cur = db.collection("converted", {
-         primaryKey : 'CURRENCY'
-     });
+  }
+        db_cur = db.collection("converted", {
+            primaryKey : 'CURRENCY'
+        });
 
-     db_cur.load();
+
+
+        db_cur.load();
+
+
+        db_cur.save();
+
 
 }
 init_db();
